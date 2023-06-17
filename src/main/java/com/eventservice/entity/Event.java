9d527b9349e4701cb25ba1ceb9b9f16500a022aa
Julio Valverde <jvalverde89@gmail.com>
2023-06-17 12:13:45 -0500

    Primera subida, creadas las entidades.

---
+++ src/main/java/com/eventservice/entity/Event.java
@@ -0,0 +1,110 @@
+package com.eventservice.entity;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+@Entity
+@Table(name = "t_event")
+
+public class Event {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String service;
+	private String eventType;
+	private Date eventDate;
+	private String detail;
+	
+	@JsonBackReference(value = "user")
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+	
+	public Event() {
+		super();
+	}
+
+	public Event(Long id, String service, String eventType, Date eventDate, String detail) {
+		super();
+		this.id = id;
+		this.service = service;
+		this.eventType = eventType;
+		this.eventDate = eventDate;
+		this.detail = detail;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+	public Date getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(Date eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/eventservice/entity/User.java
@@ -0,0 +1,106 @@
+package com.eventservice.entity;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "t_user", uniqueConstraints = {@UniqueConstraint(columnNames = {"company","usercode"}) })
+
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String company;
+	private String usercode;
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Event> events;
+	
+	public User() {
+		super();
+	}
+	public User(Long id, String company, String usercode, String name) {
+		super();
+		this.id = id;
+		this.company = company;
+		this.usercode = usercode;
+		this.name = name;
+	}
+	
+	
+	public User(Long id, String company, String usercode, String name, List<Event> events) {
+		super();
+		this.id = id;
+		this.company = company;
+		this.usercode = usercode;
+		this.name = name;
+		this.events = events;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getUsercode() {
+		return usercode;
+	}
+	public void setUsercode(String usercode) {
+		this.usercode = usercode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	public List<Event> getEvents() {
+		return events;
+	}
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(company, id, name, usercode);
+	}
+	
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(company, other.company) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(usercode, other.usercode);
+	}
+	
+	
+	
+
+}
